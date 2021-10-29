0d01eb27b191a285b410e0e77b310bced844d4d1
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-10-29 22:26:54 +0530

    capacity resize shrink_to_fit length

---
+++ String/capacity resize length.cpp
@@ -0,0 +1,45 @@
+4. capacity() :- This function returns the capacity allocated to the string, which can be equal to or more than the size of the string. Additional space is allocated so that when the new characters are added to the string, the operations can be done efficiently.
+5. resize() :- This function changes the size of string, the size can be increased or decreased.
+6.length():-This function finds the length of the string
+7.shrink_to_fit() :- This function decreases the capacity of the string and makes it equal to the minimum capacity of the string. This operation is useful to save additional memory if we are sure that no further addition of characters have to be made.
+
+// C++ code to demonstrate the working of
+// capacity(), resize() and shrink_to_fit()
+#include<iostream>
+#include<string> // for string class
+using namespace std;
+int main()
+{
+	// Initializing string
+	string str = "geeksforgeeks is for geeks";
+
+	// Displaying string
+	cout << "The initial string is : ";
+	cout << str << endl;
+
+	// Resizing string using resize()
+	str.resize(13);
+
+	// Displaying string
+	cout << "The string after resize operation is : ";
+	cout << str << endl;
+
+	// Displaying capacity of string
+	cout << "The capacity of string is : ";
+	cout << str.capacity() << endl;
+
+	//Displaying length of the string
+	cout<<"The length of the string is :"<<str.length()<<endl;
+
+	// Decreasing the capacity of string
+	// using shrink_to_fit()
+	str.shrink_to_fit();
+
+	// Displaying string
+	cout << "The new capacity after shrinking is : ";
+	cout << str.capacity() << endl;
+
+	return 0;
+
+}
+
