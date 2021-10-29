218d2f6fc8c7c9324a5c460251c34b9d968a9fc1
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-10-29 20:59:47 +0530

    Most important

---
+++ Algorithm/Most Important.cpp
@@ -0,0 +1,27 @@
+sort(first_iterator, last_iterator) – To sort the given vector.
+
+reverse(first_iterator, last_iterator) – To reverse a vector.
+
+*max_element (first_iterator, last_iterator) – To find the maximum element of a vector.
+
+*min_element (first_iterator, last_iterator) – To find the minimum element of a vector.
+
+accumulate(first_iterator, last_iterator, initial value of sum) – Does the summation of vector elements
+
+count(first_iterator, last_iterator,x) – To count the occurrences of x in vector.
+
+find(first_iterator, last_iterator, x) – Returns an iterator to the first occurence of x in vector and points to last address of vector ((name_of_vector).end()) if element is not present in vector.
+
+distance(first_iterator,desired_position) – It returns the distance of desired position from the first iterator.This function               is very useful while finding the index.
+
+next_permutation(first_iterator, last_iterator) – This modified the vector to its next permutation.
+
+prev_permutation(first_iterator, last_iterator) – This modified the vector to its previous permutation. 
+
+
+
+
+
+
+
+link -- https://www.geeksforgeeks.org/algorithms-library-c-stl/
