708f63f6ed07ad735443bfcb1da9d5ef3639b4ff
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-11-01 16:13:25 +0530

    Vector in vector

---
+++ Vector/vector in vector/Intro.cpp
@@ -0,0 +1,10 @@
+Vectors are known as dynamic arrays with the ability to resize itself automatically when an element is inserted or deleted, with their storage being handled automatically by the container.
+Vector of Vectors is a two-dimensional vector with a variable number of rows where each row is vector. Each index of vector stores a vector which can be traversed and accessed using iterators. It is similar to an Array of Vectors but with dynamic properties.
+
+Syntax:
+
+
+
+vector<vector<data_type>> vec;
+
+
