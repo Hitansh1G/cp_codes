0cc2536f26fb388e721e385fcd3a27a46e674377
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-11-14 15:49:03 +0530

    introduction to stringstream

---
+++ String/stringstream in C++.cpp/introduction.cpp
@@ -0,0 +1,5 @@
+A stringstream class in C++ is a Stream Class to Operate on strings. The stringstream class Implements the Input/Output Operations on Memory Bases streams i.e. string:
+
+The stringstream class in C++ allows a string object to be treated as a stream. It is used to operate on strings. By treating the strings as streams we can perform extraction and insertion operation from/to string just like cin and cout streams.
+
+These types of operations are mostly useful to convert string to numerical data types and vice versa. The stringstream class also proves to be helpful in different types of parsing.
